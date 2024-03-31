ef45b5ac4e75953cbcefc9964ebdac3130b2e466
cayan701 <cayan701@gmail.com>
2024-03-31 23:03:30 +0530

    Add new files

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-const adminAuthentication = (req, res, next) => {
+const adminAuthentication = (req, res, next) => { 
     const { username, password } = req.headers;
-    const admin = ADMINS.find((a) => a.username === admin.username && a.password === admin.password);
+    const admin = ADMINS.find((a) => a.username === username && a.password === password);
     if(admin) {
         next();
     } else {
